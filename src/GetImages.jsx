b2f44a61dac0e77fadea04c8032f7fc939c06801
babu97 <benkaimugul@gmail.com>
2023-11-09 08:35:12 +0300

    added fetchimages function

--- src/GetImages.jsx
+++ src/GetImages.jsx
@@ -2,6 +2,21 @@ import { useState,useEffect } from "react"
 export default function GetImages(){
     const [images, setImages] = useState([])
 
+    const fetchImages = async () => {
+        try {
+          const res = await fetch(`https://api.unsplash.com/photos?client_id=${process.env.REACT_APP_UNSPLASH_API_KEY}`);
+          const data = await res.json();
+          setImages(data);
+        } catch (error) {
+          console.error("Error fetching images:", error);
+        }
+      };
+      
+      useEffect(() => {
+        fetchImages();
+      }, []);
+      
+
     return(
         <h1>UserEffect</h1>
     )
