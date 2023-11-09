84625df74e98f0ad27effe93c4c09dd163266159
babu97 <benkaimugul@gmail.com>
2023-11-09 23:48:15 +0300

    added poster details

--- src/Image.jsx
+++ src/Image.jsx
@@ -1,6 +1,16 @@
-export default function Image(){
+export default function Image(props){
 
     return(
-        <h2>Images</h2>
+        <>
+        <article className="shadow-md bg-white rounded-3xl  p-5">
+            <img src={props.urls.full} alt={props.user.name} loading="lazy"  className="h-52  w-full  object-cover  rounded-3xl
+            md:h-80 "/>
+            <div className="pt-5">
+                 <img src={props.user.profile_image.large} alt  = {props.user.name} loading="lazy"
+                 className="w-20 rounded-full shadow "/>
+            </div>
+
+        </article>
+        </>
     )
 }
