2b76544b61e22ca9bd361ff4313c0d9dc145d4f1
babu97 <benkaimugul@gmail.com>
2023-11-09 23:23:18 +0300

    styled the section

--- src/GetImages.jsx
+++ src/GetImages.jsx
@@ -21,17 +21,18 @@ export default function GetImages(){
 
     return(
         <>
-       {!images ? <h2 className="flex items-center justify-center h-screen font-bold text-4xl
-       text-center text-slate-800">Loading...</h2>:
-       <section>
-        <h1> Recommended for you </h1>
+       {!images ? (<h2 className="flex items-center justify-center h-screen font-bold text-4xl
+       text-center text-slate-800">Loading...</h2>):
+       (<section className="px-5 container mx-auto">
+        <h1 className="font-bold text-3xl md:text-4xl lg:text-6xl my-10 lg:mt-20  lg:md-16 text-slate-800"> 
+        Recommended for you </h1>
         <div>
             {images.map((image)=>(
              <Image key={image.id}{...image}/>
             ))}
 
         </div>
-       </section>
+       </section>)
 
        }
        </>
