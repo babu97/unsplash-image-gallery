4a455ab6b039f0d50dadbd75715d8b2507791edc
babu97 <benkaimugul@gmail.com>
2023-11-10 09:58:48 +0300

    added server.js to serve my backend

--- src/GetImages.jsx
+++ src/GetImages.jsx
@@ -1,67 +1,65 @@
-import React, { useState, useEffect } from "react";
-import Image from "./Image";
+import React, { useState, useEffect } from 'react';
+import Image from './Image';
 
 export default function GetImages() {
   const [images, setImages] = useState([]);
-  const [searchInput, setSearchInput] = useState("");
-  const [filteredImages, setFilteredImages] = useState([]);
+  const [searchInput, setSearchInput] = useState('');
 
-  const fetchImages = async () => {
+  const fetchImages = async (query) => {
     try {
-      const res = await fetch(
-        `https://api.unsplash.com/photos?client_id=${process.env.REACT_APP_UNSPLASH_API_KEY}`
-      );
+      const res = await fetch(`/api/search`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ query }),
+      });
+
       const data = await res.json();
       setImages(data);
-      setFilteredImages(data); // Initially, set filteredImages to all images
     } catch (error) {
-      console.error("Error fetching images:", error);
+      console.error('Error fetching images:', error);
     }
   };
 
-  useEffect(() => {
-    fetchImages();
-  }, []);
-
-  // Function to handle search input changes
   const handleSearchInputChange = (e) => {
-    const inputValue = e.target.value;
-    setSearchInput(inputValue);
-
-    // Filter images based on search input
-    const filtered = images.filter((image) =>
-      image.alt_description.toLowerCase().includes(inputValue.toLowerCase())
-    );
-    setFilteredImages(filtered);
+    setSearchInput(e.target.value);
   };
+
+  const handleSearch = () => {
+    fetchImages(searchInput);
+  };
+
+  useEffect(() => {
+    // Fetch default images when the component mounts
+    fetchImages('default');
+  }, []);
 
   return (
     <>
-      {!images ? (
-        <h2 className="flex items-center justify-center h-screen font-bold text-4xl text-center text-slate-800">
-          Loading...
-        </h2>
-      ) : (
-        <section className="px-5 container mx-auto">
-          <div className="flex items-center justify-center my-5">
-            <input
-              type="text"
-              placeholder="Search images..."
-              value={searchInput}
-              onChange={handleSearchInputChange}
-              className="p-2 border border-gray-300 rounded-md"
-            />
-          </div>
-          <h1 className="font-bold text-3xl md:text-4xl lg:text-6xl my-10 lg:mt-20  lg:md-16 text-slate-800">
-            Recommended for you
-          </h1>
-          <div className="grid grid-cols-1 gap-10 md:grid-cols-2 lg:grid-cols-3">
-            {filteredImages.map((image) => (
-              <Image key={image.id} {...image} />
-            ))}
-          </div>
-        </section>
-      )}
+      <div className="flex items-center justify-center my-5">
+        <input
+          type="text"
+          placeholder="Search images..."
+          value={searchInput}
+          onChange={handleSearchInputChange}
+          className="p-2 border border-gray-300 rounded-md"
+        />
+        <button onClick={handleSearch} className="ml-2 p-2 bg-blue-500 text-white rounded-md">
+          Search
+        </button>
+      </div>
+
+      <section className="px-5 container mx-auto">
+        <h1 className="font-bold text-3xl md:text-4xl lg:text-6xl my-10 lg:mt-20  lg:md-16 text-slate-800">
+          Recommended for you
+        </h1>
+        <div className="grid grid-cols-1 gap-10 md:grid-cols-2 lg:grid-cols-3">
+          {images.map((image) => (
+            <Image key={image.id} {...image} />
+          ))}
+        </div>
+      </section>
     </>
   );
 }
